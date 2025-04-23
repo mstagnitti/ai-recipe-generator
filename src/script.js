02cd8ce995c40dd17df9d64431e2be4394cdd852
mstagnitti <monique@learntoplaytherapy.com>
2025-04-23 14:53:12 +1000

    Added JS form handling and typewriter plugin

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "Generating recipe for you...",
+    autoStart: true,
+    cursor: "",
+    delay: 40,
+  });
+}
+
+let inputFormElement = document.querySelector("#recipe-form");
+inputFormElement.addEventListener("submit", generateRecipe);
