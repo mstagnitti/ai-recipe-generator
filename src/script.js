3f87b7093ae762933cf38b7711c42ef67fedf348
mstagnitti <monique@learntoplaytherapy.com>
2025-04-24 09:55:36 +1000

    Fixed null input value return

--- src/script.js
+++ src/script.js
@@ -3,15 +3,13 @@ function displayRecipe(response) {
     strings: response.data.answer,
     autoStart: true,
     cursor: "",
-    delay: 20,
+    delay: 15,
   });
 }
 
 function generateRecipe(event) {
   event.preventDefault();
   let ingredientElement = document.querySelector("#ingredient");
-  let ingredientValue = ingredientElement.value;
-  console.log(document.querySelector("#ingredient"));
 
   let apiKey = "aa50c5taa349ad0ccb0b41133of3fd60";
   let prompt = `User instructions: create a simple and delicious recipe making sure to include ${ingredientElement.value} in the recipe. Display the recipe with the ingredient list first and then the method. Create a heading for the ingredient list using the  HTML format <h3> and put each ingredient on a new line using  HTML format <br />. Create a heading for the method using the  HTML format <h3> and display each sentence on a new line using the HTML format <br />. Only provide the title of the recipe using the format <h2>, the ingredient list and the method. Use metric measurements for the ingredients`;
@@ -24,7 +22,7 @@ function generateRecipe(event) {
     strings: "Generating recipe for you...",
     autoStart: true,
     cursor: "",
-    delay: 80,
+    delay: 100,
   });
 }
 
